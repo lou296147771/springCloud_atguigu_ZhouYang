932664dac31d35bc63435751e457a8a626e4ee80
cyhbyw <swust2009cyh@qq.com>
2018-06-07 21:03:29 +0800

    add module 'microservicecloud-consumer-dept-80'

---
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/cyh/spring/cloud/DeptConsumer80_App.java
@@ -0,0 +1,12 @@
+package com.cyh.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_App.class, args);
+    }
+}
---
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/cyh/spring/cloud/controller/DeptController_Consumer.java
@@ -0,0 +1,39 @@
+package com.cyh.spring.cloud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.cyh.spring.cloud.entity.Dept;
+
+@RestController
+public class DeptController_Consumer {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public boolean add(Dept dept) {
+        System.out.println("消费者添加一个Dept: " + dept);
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable Long id) {
+        System.out.println("消费者查询Dept，id: " + id);
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list() {
+        System.out.println("消费者查询所有Dept列表");
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
+}
