3f75445fb459e66c2cb0a699a21d3f623a60acc0
cyhbyw <swust2009cyh@qq.com>
2018-06-07 21:22:43 +0800

    register 'microservicecloud-provider-dept-8001' into 'microservicecloud-eureka-7001'

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/cyh/spring/cloud/DeptProvider8001_App.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/cyh/spring/cloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.cyh.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
 
     public static void main(String[] args) {
