617f80842e5eac85f270761a29aa2fb5f85d5ce5
cyhbyw <swust2009cyh@qq.com>
2018-06-10 10:17:56 +0800

    create module 'microservicecloud-config-eureka-7001'

---
+++ microservicecloud/microservicecloud-config-eureka-7001/src/main/java/com/cyh/spring/cloud/Config_Git_EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.cyh.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Config_Git_EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Git_EurekaServerApplication.class, args);
+    }
+}
