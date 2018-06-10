a4cf8e6012c85ab6990b16494e1e901936913292
cyhbyw <swust2009cyh@qq.com>
2018-06-10 10:02:49 +0800

    create module 'microservicecloud-config-client-3355'

---
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/com/cyh/spring/cloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,12 @@
+package com.cyh.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
